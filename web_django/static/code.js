e85b5167b5ada355c1632748d9a28aad10fdf56f
Ilgam <ilgamgabdullin@yandex.ru>
2022-04-06 22:46:49 +0300

    templates

---
+++ web_django/static/code.js
@@ -0,0 +1,11 @@
+const button = document.querySelector('button');
+const video = document.querySelector('video');
+
+button.addEventListener('click', (e) => {
+    navigator.mediaDevices.getUserMedia({video: true, audio: true}).then((stream) => {
+        video.srcObject = stream;
+        video.play();
+    }).catch((error) => {
+        console.log('error', error)
+    })
+})
---
+++ web_django/views.py
@@ -0,0 +1,8 @@
+from django.http import JsonResponse
+from django.template import loader
+
+from django.shortcuts import render
+
+
+def index(request):
+    return render(request, 'index.html')
